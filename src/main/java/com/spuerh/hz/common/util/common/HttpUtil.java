6ffb5c69efad2797a79bf817b48df6fe6ef9542c
kasoul <ccsysyx@sina.com>
2016-09-19 18:05:47 +0800

    update：HttpUtil

--- src/main/java/com/spuerh/hz/common/util/common/HttpUtil.java
+++ src/main/java/com/spuerh/hz/common/util/common/HttpUtil.java
@@ -5,21 +5,41 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.GZIPInputStream;
 
+import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.commons.io.IOUtils;
 
-
 /** 
  * HTTP 获取网页地址
- * 2014-10-8
  */
-
 public class HttpUtil {
+	
+	/**
+	 * get方式获取url内容,兼容重定向
+	 * @param url 网址
+	 * @return 网页内容
+	 * @throws Exception
+	 */
+	public static String fetchGetURLContentRedirect(String url) throws Exception {
+		return fetchURLContentRedirect(url, "UTF-8", "GET",null);
+	}
+	
+	/**
+	 * get方式获取url内容
+	 * @param url 网址
+	 * @return 网页内容
+	 * @throws Exception
+	 */
+	public static String fetchGetURLContent(String url) throws Exception {
+		return fetchURLContent(url, "UTF-8", "GET",null);
+	}
 	
 	/**
 	 * get方式获取url内容
@@ -108,7 +128,7 @@ public class HttpUtil {
 		return ret;
 	}
 	
-	private static String fetchURLContent(String url, String charset, String methodType, NameValuePair... params){
+	private static String fetchURLContent(String url, String charset, String methodType, NameValuePair... params) throws HttpException, IOException{
 
 		HttpClient client = new HttpClient();
 		
@@ -125,45 +145,113 @@ public class HttpUtil {
 		method.getParams().setContentCharset("GBK");
 		method.setRequestHeader("Accept-Language", "zh-cn");
 		method.setRequestHeader("Accept-Encoding", " gzip, deflate");
-		try {
-			int ctimeout = 5000;
-			int stimeout = 55000;
-			client.getHttpConnectionManager().getParams().setConnectionTimeout(ctimeout);
-			client.getHttpConnectionManager().getParams().setSoTimeout(stimeout);
-			client.executeMethod(method);
-			String acceptEncoding = "";
-			try {
-				acceptEncoding = method.getResponseHeader("Content-Encoding").getValue();
-			} catch (Exception e) {
-				//e.printStackTrace();
-			}
 
-			String ret;
-			if (acceptEncoding.toLowerCase().indexOf("gzip") > -1) 
-			{
-				InputStream is = method.getResponseBodyAsStream();
-				GZIPInputStream gzin = new GZIPInputStream(is);
-				java.io.ByteArrayOutputStream bos = new java.io.ByteArrayOutputStream();
-				IOUtils.copy(gzin, bos);
-				ret = bos.toString(charset);
-				bos.close();
-				gzin.close();
-			} else {
-				InputStream is = method.getResponseBodyAsStream();
-				java.io.ByteArrayOutputStream bos = new java.io.ByteArrayOutputStream();
-				IOUtils.copy(is, bos);
-				ret = bos.toString(charset);
-				bos.close();
-				is.close();
-			}
+		int ctimeout = 5000;
+		int stimeout = 55000;
+		client.getHttpConnectionManager().getParams().setConnectionTimeout(ctimeout);
+		client.getHttpConnectionManager().getParams().setSoTimeout(stimeout);
+		client.executeMethod(method);
+		String acceptEncoding = "";
+		acceptEncoding = method.getResponseHeader("Content-Encoding").getValue();
 
-			method.releaseConnection();
-			return ret;
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			return null;
+		String ret;
+		if (acceptEncoding.toLowerCase().indexOf("gzip") > -1) {
+			InputStream is = method.getResponseBodyAsStream();
+			GZIPInputStream gzin = new GZIPInputStream(is);
+			java.io.ByteArrayOutputStream bos = new java.io.ByteArrayOutputStream();
+			IOUtils.copy(gzin, bos);
+			ret = bos.toString(charset);
+			bos.close();
+			gzin.close();
+		} else {
+			InputStream is = method.getResponseBodyAsStream();
+			java.io.ByteArrayOutputStream bos = new java.io.ByteArrayOutputStream();
+			IOUtils.copy(is, bos);
+			ret = bos.toString(charset);
+			bos.close();
+			is.close();
 		}
+
+		method.releaseConnection();
+		return ret;
+	}
+	
+	private static String fetchURLContentRedirect(String url, String charset, String methodType, NameValuePair... params) throws HttpException, IOException{
+
+		HttpClient client = new HttpClient();
+		
+		//client.getHostConfiguration().setProxy("192.168.200.254", 3128);
+		// 使用GET方法，如果服务器需要通过HTTPS连接，那只需要将下面URL中的http换成https
+		HttpMethod method;
+		if ("GET".equals(methodType))
+			method = new GetMethod(url);
+		else
+			method = new PostMethod(url);
+		if (params !=null && params.length > 0)
+			method.setQueryString(params);
+		//method.setFollowRedirects(true);
+		method.getParams().setContentCharset("GBK");
+		method.setRequestHeader("Accept-Language", "zh-cn");
+		method.setRequestHeader("Accept-Encoding", " gzip, deflate");
+
+		int ctimeout = 5000;
+		int stimeout = 55000;
+		client.getHttpConnectionManager().getParams().setConnectionTimeout(ctimeout);
+		client.getHttpConnectionManager().getParams().setSoTimeout(stimeout);
+		client.executeMethod(method);
+		
+	    //检查是否重定向
+        int statuscode = method.getStatusCode();
+        if ((statuscode == HttpStatus.SC_MOVED_TEMPORARILY) ||
+            (statuscode == HttpStatus.SC_MOVED_PERMANENTLY) ||
+            (statuscode == HttpStatus.SC_SEE_OTHER) ||
+            (statuscode == HttpStatus.SC_TEMPORARY_REDIRECT)) {
+        	//读取新的URL地址
+            Header header = method.getResponseHeader("location");
+            if (header != null) {
+                String newuri = header.getValue();
+                if ((newuri == null) || (newuri.equals("")))
+                    newuri = "/";
+                GetMethod redirect = new GetMethod(newuri);
+                client.executeMethod(redirect);
+                //System.out.println("Redirect:"+ redirect.getStatusLine().toString());
+                method = redirect;
+            }
+        }
+        
+        //检查是否刷新
+        Header refresh = method.getResponseHeader("Refresh");
+        if(refresh != null){
+        	String characteristicString = "url=";
+        	int subIndex = refresh.getValue().lastIndexOf(characteristicString) + characteristicString.length();
+        	String newuri = refresh.getValue().substring(subIndex);
+        	GetMethod redirect = new GetMethod(newuri);
+            client.executeMethod(redirect);
+            method = redirect;
+        }
+        
+		Header acceptEncoding = method.getResponseHeader("Content-Encoding");
+
+		String ret;
+		if (acceptEncoding !=null && acceptEncoding.getValue().toLowerCase().indexOf("gzip") > -1) {
+			InputStream is = method.getResponseBodyAsStream();
+			GZIPInputStream gzin = new GZIPInputStream(is);
+			java.io.ByteArrayOutputStream bos = new java.io.ByteArrayOutputStream();
+			IOUtils.copy(gzin, bos);
+			ret = bos.toString(charset);
+			bos.close();
+			gzin.close();
+		} else {
+			InputStream is = method.getResponseBodyAsStream();
+			java.io.ByteArrayOutputStream bos = new java.io.ByteArrayOutputStream();
+			IOUtils.copy(is, bos);
+			ret = bos.toString(charset);
+			bos.close();
+			is.close();
+		}
+
+		method.releaseConnection();
+		return ret;
 	}
 	
 	public static void main(String[] args) {
