2657b12510af771c7a94a13f3eeb7f37a2b8c5f1
kasoul <ccsysyx@sina.com>
2016-10-14 19:50:44 +0800

    add:testmysql

---
+++ src/test/java/com/superh/hz/util/dbtool/TestMysql.java
@@ -0,0 +1,51 @@
+package com.superh.hz.util.dbtool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.superh.hz.common.util.dbtool.MySqlOpration;
+import com.superh.hz.common.util.dbtool.MySqlUtil;
+
+public class TestMysql {
+	
+	public static void main(String args[]){
+		testInsertBatch();
+		testInsert();
+	}
+
+
+	public static void testInsertBatch(){
+		MySqlOpration mo = new MySqlOpration(
+				"jdbc:mysql://localhost/personal_account", "root", "1111");
+		MySqlUtil.setConnectionParam("jdbc:mysql://localhost/personal_account", "root", "1111");
+		List<String> batchSql = new ArrayList<String>();
+		for(int i=0;i<5000;i++){
+			String isertSql = "insert into `test_table` (`id`,`name`)"
+					+ "VALUES(" + i + ",\'aaa\')";
+			batchSql.add(isertSql);
+		}
+		long startTime = System.currentTimeMillis();
+		mo.insertBatch(batchSql);
+		long endTime = System.currentTimeMillis();
+		System.out.println("批量插入时间：" + (endTime-startTime)/1000f);
+	}
+
+
+
+	public static void testInsert(){
+		MySqlOpration mo = new MySqlOpration(
+				"jdbc:mysql://localhost/personal_account", "root", "1111");
+		MySqlUtil.setConnectionParam("jdbc:mysql://localhost/personal_account", "root", "1111");
+		List<String> batchSql = new ArrayList<String>();
+		long startTime = System.currentTimeMillis();
+		for(int i=0;i<5;i++){
+			String isertSql = "insert into `test_table` (`id`,`name`)"
+					+ "VALUES(" + i + ",\'aaa\')";
+			//batchSql.add(isertSql);
+			mo.insert(isertSql);
+		}
+		//mo.insertBatch(batchSql);
+		long endTime = System.currentTimeMillis();
+		System.out.println("单条插入时间：" + (endTime-startTime)/1000f);
+	}
+}
